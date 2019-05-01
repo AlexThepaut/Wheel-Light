77bf04457c042482cec9134862833a5c2ab22860
Alex <alex.thepaut33@gmail.com>
2019-05-01 10:25:45 +0200

    Ajout de la preview du text à envoyer

--- src/app/send-text/send-text.component.ts
+++ src/app/send-text/send-text.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { TextPreview } from '../model/text-preview.model';
+import { environment } from 'src/environments/environment.prod';
 
 @Component({
   selector: 'app-send-text',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SendTextComponent implements OnInit {
 
+  textToSend = "";
+  textPreview: TextPreview[];
+  defaultColor = "rgb(255, 0, 0)";
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  handlePreview() {
+    this.textPreview = []
+    let partSplit = this.textToSend.split("*");
+    partSplit.forEach((p) => {
+      let colorSplit = p.split("]");
+      let color = (colorSplit.length > 1) ? `rgb(${colorSplit[0].substr(1)})` : this.defaultColor;
+      let text = (colorSplit.length > 1) ? colorSplit[1] : colorSplit[0];
+      this.textPreview = [... this.textPreview, new TextPreview(text, color)];
+    });
+  }
+
+  textWriting(event) {
+    this.textToSend = event.target.value;
+  }
 }
